ebc5bebb4a541da9cf15278c5b8aef31e6ad26f8
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2025-02-27 12:20:01 -0300

    Delete joalheriajoiasjoiaV1/joalheriaJoiasJoia directory

--- joalheriajoiasjoiaV1/joalheriaJoiasJoia/joalheriaJoiasJoia_v1.0/joalheriaJoiasJoia/src/main/java/br/com/joalheriajoiasjoia/app/repositories/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.joalheriajoiasjoia.app.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.joalheriajoiasjoia.app.entities.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
-
-}
