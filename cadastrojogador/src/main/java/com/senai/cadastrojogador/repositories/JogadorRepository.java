4ddc6f1a927c08d5c5a7f68d1d180efbdc4a4ed9
Keren Stevaux <159180442+kerenStevaux@users.noreply.github.com>
2025-05-08 11:27:49 -0300

    Delete cadastrojogador directory

--- cadastrojogador/src/main/java/com/senai/cadastrojogador/repositories/JogadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.cadastrojogador.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.cadastrojogador.entitites.Jogador;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {}
-
-
--- cadastrojogador/src/main/java/com/senai/cadastrojogador/repositories/JogoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senai.cadastrojogador.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.cadastrojogador.entitites.Jogo;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long> {}
